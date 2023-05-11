95e21410ab06584a31d006b0e22d2fc8629612af
mfujarewicz <michal.fujarewicz@gmail.com>
2023-05-11 21:36:23 +0200

    Segment types back to traits with added shorthands and handmade withName method

--- src/main/scala/vm/SegmentType.scala
+++ src/main/scala/vm/SegmentType.scala
@@ -1,7 +1,56 @@
 package vm
 
+import vm.SegmentTypes._
 
-object SegmentType extends Enumeration {
-  type SegmentType = Value
-  val ARGUMENT, LOCAL, STATIC, CONSTANT, THIS, THAT, POINTER, TEMP = Value
+
+//object SegmentType extends Enumeration {
+//  type SegmentType = Value
+//  val LOCAL, ARGUMENT, STATIC, CONSTANT, THIS, THAT, POINTER, TEMP = Value
+//}
+
+trait SegmentType {
+  val asmShorthand: Option[String] = None
+}
+
+object SegmentTypes {
+
+  object LOCAL extends SegmentType {
+    override val asmShorthand: Option[String] = Some("LCL")
+  }
+
+  object ARGUMENT extends SegmentType {
+    override val asmShorthand: Option[String] = Some("ARG")
+  }
+
+  object STATIC extends SegmentType
+
+  object CONSTANT extends SegmentType
+
+  object THIS extends SegmentType {
+    override val asmShorthand: Option[String] = Some("THIS")
+  }
+
+  object THAT extends SegmentType {
+    override val asmShorthand: Option[String] = Some("THAT")
+  }
+
+  object POINTER extends SegmentType
+
+  object TEMP extends SegmentType
+
+}
+
+object SegmentType {
+  def withName(s: String): SegmentType = {
+    s match {
+      case "LOCAL" => LOCAL
+      case "ARGUMENT" => ARGUMENT
+      case "STATIC" => STATIC
+      case "CONSTANT" => CONSTANT
+      case "THIS" => THIS
+      case "THAT" => THAT
+      case "POINTER" => POINTER
+      case "TEMP" => TEMP
+    }
+  }
 }
